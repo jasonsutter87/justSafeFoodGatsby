de71ddc8a7f3328fa20779f819adc8aeaa6c6f6b
Jason Sutter <jasonsutter87@gmail.com>
2023-10-25 13:31:52 -0700

    Adding home

--- src/componets/home/hero.js
+++ src/componets/home/hero.js
@@ -2,6 +2,18 @@ import React from 'react'
 
 export default function Hero() {
   return (
-    <div>Hero</div>
+    <section id="index-hero-section">
+      <div className="site-container-1340">
+          <div className="row">
+              <div className="col-12 col-md-5 t-c-l">
+                  <h1>The Online Course for Safe and Sustainable Food Service</h1>
+                  <h6 className="d-none d-lg-block">Train employees on how to handle, prepare, and serve food safely without the stress.</h6>
+                  <div className="py-4">
+                      <a href="/start-now" className="btn-primary btn-primary-alt btn-size-lg">Get Started</a>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </section>
   )
 }
